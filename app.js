c97826bffcff4a534bccf4f3521af787ed2a8c3c
Henry Cuffy <hcuffy_5@hotmail.com>
2018-03-17 16:55:42 +0100

    Trying redux

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
 import { createStore } from 'redux';
+import { ADD_RECIPE } from './action-types';
+import { ADD_INGREDIENT } from './action-types';
 
-const reducer = (state, action) =>
-	action.type === 'INC'
-		? state + 1
-		: state;
+const initialState = {
+  recipes: [{
+    name: 'Omelette'
+  }],
+  ingredients: [{
+    recipe: 'Omelette',
+    name: 'Egg',
+    quantity: 2
+  }]
+};
 
-const initialState = 0;
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ADD_RECIPE:
+      return Object.assign({}, state, {
+        recipes: state.recipes.concat({
+          name: action.name
+        })
+      });
+    case ADD_INGREDIENT:
+      const newIngredient = {
+        name: action.name,
+        recipe: action.recipe,
+        quantity: action.quantity
+      };
+      return Object.assign({}, state, {
+        ingredients: state.ingredients.concat(newIngredient)
+      });
+  }
+
+return state;
+};
 
 const store = createStore(reducer, initialState);
 
 store.subscribe(() => document.getElementById('counter').innerText = store.getState());
 
-setInterval(() => store.dispatch({ type: 'INC' }), 500);
+// window.store = store;
+
 
 console.log("Redux started");
